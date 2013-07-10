efa037b37ab28417285c0b3abd921561cd34de85
Mark Smullen <mark@techfocus.net>
2013-07-09 23:06:22 -0500

    Modularized messageListener, better for maintenance.

--- src/MessageListener.java
+++ src/MessageListener.java
@@ -7,33 +7,21 @@ public class MessageListener extends ListenerAdapter
 {	
 	public void onPrivateMessage(PrivateMessageEvent e)
 	{
-		if(e.getMessage().startsWith("!help"))
-		{
-			e.getUser().sendMessage("Available Commands:");
-			e.getUser().sendMessage("!site,!website - links cubeworld");
-			e.getUser().sendMessage("!status - link the status site");
-			e.getUser().sendMessage("!techdemo, !demo - links the world demo");
-		}
-		
-		if(e.getMessage().startsWith("!status"))
-		{
-			e.getUser().sendMessage("The Statuses for the CubeWorld systems may be found at http://direct.cyberkitsune.net/canibuycubeworld/");
-		}
-		
-		if(e.getMessage().startsWith("!website") || e.getMessage().startsWith("!site"))
-		{
-			e.getUser().sendMessage("CubeWorld may be found at https://picroma.com/cubeworld");
-		}
-		
-		if(e.getMessage().startsWith("!techdemo") || e.getMessage().startsWith("!demo"))
-		{
-			e.getUser().sendMessage("The Tech demo was linked to in this post https://picroma.com/blog/post/6");
-		}
+		checkMessages(e);
 	}
 	
 	public void onPrivateMessage(NoticeEvent e)
 	{
-		if(e.getMessage().startsWith("!help"))
+		checkMessages(e);
+	}
+	
+	public void onMessage(MessageEvent e)
+	{	
+		checkMessages(e);
+	}
+        
+        private void checkMessages(org.pircbotx.hooks.types.GenericMessageEvent e) {
+            if(e.getMessage().startsWith("!help"))
 		{
 			e.getUser().sendMessage("Available Commands:");
 			e.getUser().sendMessage("!site,!website - links cubeworld");
@@ -55,31 +43,5 @@ public class MessageListener extends ListenerAdapter
 		{
 			e.getUser().sendMessage("The Tech demo was linked to in this post https://picroma.com/blog/post/6");
 		}
-	}
-	
-	public void onMessage(MessageEvent e)
-	{	
-		if(e.getMessage().startsWith("!help"))
-		{
-			e.getUser().sendMessage("Available Commands:");
-			e.getUser().sendMessage("!site, !website - links cubeworld");
-			e.getUser().sendMessage("!status, .status - links the status site");
-			e.getUser().sendMessage("!techdemo, !demo - links the world demo");
-		}
-		
-		if(e.getMessage().startsWith("!status"))
-		{
-			e.getChannel().sendMessage("The Statuses for the CubeWorld systems may be found at http://direct.cyberkitsune.net/canibuycubeworld/");
-		}
-		
-		if(e.getMessage().startsWith("!website") || e.getMessage().startsWith("!site"))
-		{
-			e.getChannel().sendMessage("CubeWorld may be found at https://picroma.com/cubeworld");
-		}
-		
-		if(e.getMessage().startsWith("!techdemo") || e.getMessage().startsWith("!demo"))
-		{
-			e.getChannel().sendMessage("The Tech demo was linked to in this post https://picroma.com/blog/post/6");
-		}
-	}
+        }
 }
